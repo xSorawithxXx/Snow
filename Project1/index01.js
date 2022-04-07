523ca411254e7c50e0cfb975e8fa27707a679d0e
Haru <Sotawit@igeargeek.com>
2022-04-07 17:40:26 +0700

    .gitignore fix

--- Project1/index01.js
+++
@@ -1,4 +0,0 @@
-let age = prompt("กรุณาใส่อายุของคุณ")
-if(age>13){
-    document.getElementById("Haru").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/PGZ7QiKdumo\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
-}
--- Project5/index.js
+++
@@ -1,7 +0,0 @@
-//onclike เมือคลิกจะเริ่มทำงาน -> อีเว้น
-//button สร้างปุ่ม
-
-function sayhello(){
-    alert("Hellooooooooooooooo")
-
-}
